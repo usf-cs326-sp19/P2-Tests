006c5dc23d98aae96c802db8ecc07d7933a89334
Matthew Malensek <matt@malensek.net>
2019-03-16 12:40:12 -0700

    Add note about trailing spaces

--- 01-Scripting-1.sh
+++ 01-Scripting-1.sh
@@ -1,6 +1,7 @@
 source "${TEST_DIR}/funcs.bash"
 
-test_start "Scripting Support"
+test_start "Scripting Support" \
+    "Trailing spaces should be eliminated by your command line tokenization process"
 
 expected=$(sh < "${TEST_DIR}/scripts/inspector.sh" 2> /dev/null)
 actual=$(timeout 2 ./$SHELL_NAME < "${TEST_DIR}/scripts/inspector.sh" 2> /dev/null) || test_end
