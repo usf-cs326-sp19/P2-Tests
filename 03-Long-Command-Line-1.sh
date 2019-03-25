34e3dd32e2b94c0c1f697bad8999a6b65d0cf3f1
Matthew Malensek <matt@malensek.net>
2019-03-25 10:01:00 -0700

    Increase timeout for long command line test

--- 03-Long-Command-Line-1.sh
+++ 03-Long-Command-Line-1.sh
@@ -2,9 +2,9 @@ source "${TEST_DIR}/funcs.bash"
 
 test_start "Long command lines"
 
-timeout 5 ./$SHELL_NAME < "${TEST_DIR}/scripts/too-long.sh" &> /dev/null || test_end 1
+timeout 15 ./$SHELL_NAME < "${TEST_DIR}/scripts/too-long.sh" &> /dev/null || test_end 1
 expected=$(sh < "${TEST_DIR}/scripts/long.sh" 2> /dev/null)
-actual=$(timeout 5 ./$SHELL_NAME < "${TEST_DIR}/scripts/long.sh" 2> /dev/null) || test_end 1
+actual=$(timeout 15 ./$SHELL_NAME < "${TEST_DIR}/scripts/long.sh" 2> /dev/null) || test_end 1
 
 compare <(echo "${expected}") <(echo "${actual}")
 
